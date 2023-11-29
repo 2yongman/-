20ba150c305006b69d17b81053a8c5ab88226f39
이용만 <dydaks7878@gmail.com>
2023-11-29 15:42:53 +0900

    @SpringbootTest 어노테이션 삭제

--- src/test/java/com/test/rmsoft/RmSoftApplicationTests.java
+++ src/test/java/com/test/rmsoft/RmSoftApplicationTests.java
@@ -3,7 +3,6 @@ package com.test.rmsoft;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class RmSoftApplicationTests {
 
     @Test
